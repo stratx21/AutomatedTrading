2d70cd5ce74f9c1494174b20d7e3bb88e2bd718e
Josh <stratx21@gmail.com>
2021-08-21 13:07:53 -0500

    added listen to server

--- Backtesting/DelegationNetwork/DelegationServer.py
+++ Backtesting/DelegationNetwork/DelegationServer.py
@@ -1,11 +1,9 @@
 import Backtesting.DelegationNetwork.server_config as server_config
 import socket
-import sys
-import argparse
 from _thread import start_new_thread
 
 def clientConnectionThreadHandler(connection):
-    connection.send(str.encode('Welcome to the Servern'))
+    connection.send(str.encode('Welcome to the Server'))
     while 1:
         data = connection.recv(2048)
         reply = 'Server Says: ' + data.decode('utf-8')
@@ -27,6 +25,7 @@ def runDelegationServer():
     threadCount = 0
 
     while 1:
+        serverSocket.listen(20)
         Client, address = serverSocket.accept()
         print("client at", address[0] + ":" + str(address[1]), "connected")
         start_new_thread(clientConnectionThreadHandler, (Client, ))
