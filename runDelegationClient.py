5e067df31e73db58f7244b93417f5fffa7d5055c
Josh <stratx21@gmail.com>
2021-08-21 13:04:14 -0500

    added delegation script run

---
+++ runDelegationClient.py
@@ -0,0 +1,4 @@
+from Backtesting.DelegationNetwork.DelegationClient import runDelegationClient
+
+if __name__ == '__main__':
+    runDelegationClient()
---
+++ runDelegationServer.py
@@ -0,0 +1,4 @@
+from Backtesting.DelegationNetwork.DelegationServer import runDelegationServer
+
+if __name__ == '__main__':
+    runDelegationServer()
