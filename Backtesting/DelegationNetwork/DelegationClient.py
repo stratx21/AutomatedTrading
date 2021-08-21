7f48f653621a15ebc4dca1cdedc7e758369ed459
Josh <stratx21@gmail.com>
2021-08-21 15:20:50 -0500

    updated client with receiving larger messages

--- Backtesting/DelegationNetwork/DelegationClient.py
+++ Backtesting/DelegationNetwork/DelegationClient.py
@@ -3,7 +3,29 @@ import Backtesting.DelegationNetwork.server_config as delegation_server_config
 import socket 
 import time 
 import datetime
+import struct
 from Tools.StringTools import secondsToTimeDescription
+
+BUFFER_SIZE = 4096
+
+def recv_msg(sock):
+    # Read message length and unpack it into an integer
+    raw_msglen = recvall(sock, 4)
+    if not raw_msglen:
+        return None
+    msglen = struct.unpack('>I', raw_msglen)[0]
+    # Read the message data
+    return recvall(sock, msglen)
+
+def recvall(sock, length):
+    # Helper function to recv n bytes or return None if EOF is hit
+    data = bytearray()
+    while len(data) < length:
+        packet = sock.recv(length - len(data))
+        if not packet:
+            return None
+        data.extend(packet)
+    return data
 
 def runDelegationClient():
     clientSocket = socket.socket()
@@ -23,7 +45,7 @@ def runDelegationClient():
     while 1:
         inputstr = input("entry: ")
         clientSocket.send(str.encode(inputstr))
-        response = clientSocket.recv(1024)
+        response = recv_msg(clientSocket)
         print(response.decode('utf-8'))
 
 
